c24203897b39c33b3a32539393dc84901b5b9cc0
cltq <shapebrabra@gmail.com>
2026-06-23 21:27:40 +0700

    center music page content

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -3,9 +3,11 @@ import LastFmSection from "@/app/components/music/LastFmSection";
 
 export default function MusicPage() {
   return (
-    <main className="p-8 flex flex-col gap-10 max-w-lg">
-      <CurrentlyPlaying />
-      <LastFmSection />
+    <main className="flex min-h-screen items-center justify-center p-8 md:ml-[-100px]">
+      <div className="flex flex-col gap-10 max-w-lg w-full">
+        <CurrentlyPlaying />
+        <LastFmSection />
+      </div>
     </main>
   );
 }
