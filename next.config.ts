59612f0de52e4b1ca166c5df106620219f246eb0
cltq <shapebrabra@gmail.com>
2026-06-23 20:15:21 +0700

    add discord api proxy rewrite and env config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/discord/:path*",
+        destination: "https://api.applefumi.xyz/v2/discord/:path*",
+      },
+    ];
+  },
+  env: {
+    DISCORD_USERID: process.env.DISCORD_USERID,
+  },
 };
 
 export default nextConfig;
