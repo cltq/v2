697e1c9063c81df4c4f6d5145c67a1973edd0746
cltq <shapebrabra@gmail.com>
2026-06-23 20:14:20 +0700

    add central route registry

---
+++ app/routes.ts
@@ -0,0 +1,24 @@
+export interface AppRoute {
+  name: string;
+  href: string;
+  description?: string;
+}
+
+export const appRoutes: AppRoute[] = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Music",
+    href: "/music",
+  },
+  {
+    name: "Projects",
+    href: "/projects",
+  },
+  {
+    name: "GitHub",
+    href: "/github",
+  },
+];
