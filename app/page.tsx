4a23d0199658c4c1f5523bb8350350962419aca2
cltq <fuyumi.am@gmail.com>
2026-07-15 19:20:57 +0700

    fix layout by adjusting main and child component styles

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
   const userId = process.env.DISCORD_USERID ?? "";
 
   return (
-    <main className="relative grid h-full place-items-center">
+    <main className="relative grid min-h-full place-items-center px-4 md:px-8 max-md:min-h-svh">
       <div className="absolute top-12 left-0 w-full overflow-hidden pb-2 md:top-16">
         <InfiniteMarquee />
       </div>
-      <div className="flex flex-col items-center gap-6 p-8">
+      <div className="flex flex-col items-center gap-6">
         <DiscordWidget userId={userId} showSpotify={false} />
         <HeroText />
         <SocialIcons />
